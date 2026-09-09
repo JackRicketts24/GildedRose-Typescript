36568c23a9b26c23967fd437e1ef34654a5e214c
JackRicketts24 <Jack.Ricketts@softwire.com>
2026-09-09 17:25:30 +0100

    Add tests for boundary conditions.

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -15,11 +15,26 @@ describe('Gilded Rose', function () {
         expect(items[0].sellIn).to.equal(2);
     });
 
-    // it('should decrease sell-by date by 1 for generic items', function() {
-    //     const gildedRose = new GildedRose([ new Item('My Item', 20, 3) ]);
-    //     const items = gildedRose.updateQuality();
-    //     expect(items[0].sellIn).to.equal(2);
-    // });
+
+    // Tests for invalid bound values on quality and sellIn
+
+    it('Quality should not be below 0 (never be negative).', function() {
+        const gildedRose = new GildedRose([ new Item('My Item', 2, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(0);
+    });
+
+    it('Quality should not be above 50, if not Sulfuras.', function() {
+        const gildedRose = new GildedRose([ new Item('My Item', 2, 60) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(50);
+    });
+
+    it('sulfuras with quality 80 should not change', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 80, 3) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(80);
+    });
 
 
 });
