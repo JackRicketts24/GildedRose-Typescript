156a827a16c44e3abd016bbfae5551954f440516
JackRicketts24 <Jack.Ricketts@softwire.com>
2026-09-09 17:17:22 +0100

    Experimented with testing and made minor readability changes.

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,10 +3,23 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('should decrease quality by 1 for generic items', function() {
+        const gildedRose = new GildedRose([ new Item('My Item', 3, 20) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].quality).to.equal(19);
     });
+
+    it('should decrease sell-by date by 1 for generic items', function() {
+        const gildedRose = new GildedRose([ new Item('My Item', 3, 20) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(2);
+    });
+
+    // it('should decrease sell-by date by 1 for generic items', function() {
+    //     const gildedRose = new GildedRose([ new Item('My Item', 20, 3) ]);
+    //     const items = gildedRose.updateQuality();
+    //     expect(items[0].sellIn).to.equal(2);
+    // });
+
 
 });
