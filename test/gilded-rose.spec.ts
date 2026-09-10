e2d4f6d8afe9531d9a670168c7cfb9b9c1930d7f
JackRicketts24 <Jack.Ricketts@softwire.com>
2026-09-10 09:16:46 +0100

    Add tests for backstage passes.

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -50,7 +50,28 @@ describe('Gilded Rose', function () {
         expect(items[0].quality).to.equal(21);
     });
 
+ 
+    it('backstage pass quality is 0 after sell-in date', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 30) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(0); 
+    });
 
-   // Special rules for passes...  
+    it('backstage pass quality increases by 1 with more than 10 days', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 12, 20) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(21); 
+    });
 
+    it('backstage pass quality increases by 2 with less than 10 days, but more than 5 days', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 8, 20) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(22); 
+    });
+
+    it('backstage pass quality increases by 3 with less than 5 days', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 4, 20) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(23); 
+    });
 });
